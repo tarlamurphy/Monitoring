4667ce681575aadb1af2425fbf095a41635ac058
tarlamurphy <114755762+tarlamurphy@users.noreply.github.com>
2022-10-20 18:00:17 +0200

    Create rcode_ggplot2.r

---
+++ rcode_ggplot2.r
@@ -0,0 +1,42 @@
+##20/10/2022
+#learning ggplot2
+
+install.packages("ggplot2") #installs ggplot2 package
+
+library(ggplot2) #opens ggplot2
+
+virus <- c(10, 30, 40, 50, 60, 80)  #array of values measuring the virus in certain places
+death <- c(100, 240, 310, 470, 580, 690) # array of values measuring deaths
+
+plot(virus, death) #the old, simple way of doing it
+
+#you can build a table where the data corresponds - called a dataframe
+
+data.frame(virus, death) # puts the variables in a dataframe against each other
+
+#because we want to use it, we assign the dataframe to an object so we can use the object
+
+d <- data.frame(virus, death) #dataframe has been assigned to the object 'd, ready for more analysis
+
+summary(d) #summary of the object, giving univariate statistical info
+
+ggplot(d, aes(x = virus, y = death)) + #in ggplot, specify which data, the aesthetics are the variables we want to use
+  geom_point(size = 2, col = "blue") # geom_point function creates scatterplots - this is to state we want to use points
+#inside the geompoint fuction, you can change size/colour of the points. can still use pch for symbol
+     
+#can use geom_line to show lines connecting points
+ggplot(d, aes(x = virus, y = death)) + 
+  geom_line()
+
+#can use geom_line and geom_point together
+ggplot(d, aes(x = virus, y = death)) + 
+  geom_point(col = "coral", size = 3) +
+  geom_line()
+
+#instead of line or point you can also use polygons
+ggplot(d, aes(x = virus, y = death)) + 
+  geom_point(col = "coral", size = 3) +
+  geom_line() +
+  geom_polygon(col = "orange")
+
+     
