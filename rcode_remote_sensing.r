37baf0e7ff83190208d82cc5332468c3fb6fcd14
tarlamurphy <114755762+tarlamurphy@users.noreply.github.com>
2022-11-18 17:40:32 +0100

    Update rcode_remote_sensing.r

--- rcode_remote_sensing.r
+++ rcode_remote_sensing.r
@@ -115,3 +115,64 @@ plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist") # stretches the values so y
 #essential - time series. 
 #see the changes over time in the same place
 
+#################################
+
+p224r63_1988 <- brick("p224r63_1988_masked.grd") #load image from the past the same as done before
+
+p224r63_1988 # recall for summary as a check the import worked
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="lin") #plot first with the natural colour scheme
+
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="lin")
+
+plotRGB(p224r63_1988, r=3, g=4, b=2, stretch="lin")
+
+
+#exercise - plot 1988 and 2011 on top of each other for one of the versions
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=3, g=2, b=4, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+#create a new image which is the difference between the two images
+#near infrared reflects a lot
+#if reflectance is reduced in the second picture, for example a tree has been cut so there is less reflectance
+
+
+#claculate the difference = MULTITEPORAL ANALYSIS
+#difference in near infrared 1988 vs 2011
+#take the 4th element, as it is the infrared band
+difnir <- p224r63_1988[[4]] - p224r63_2011[[4]] #subtract element 4 of 2011 from 1988
+
+plot(difnir)  #plot of difference in reflectance
+
+cl <- colorRampPalette(c('red', 'orange', 'blue'))(150) #set our colour ramp - some experimentation needed
+
+plot(difnir, col=cl)
+#blue areas are areas where there have been significant changes -trees cut
+
+
+#NDVI 
+#the difference between the near infrared and red bands. 
+#differentiated vegetation index
+#plant is dying = lower nir, higher red as the plant can no longer absorb it
+#dvi = nir - red
+
+#DIFFERENCIATED VEGETATION INDEX
+
+#first the recent dvi
+par(mfrow=c(2,1))
+dvi2011 <- p224r63_2011[[4]] - p224r63_2011[[3]] #subtract red band from infrared band
+plot(dvi2011) 
+
+#then the 1988
+dvi1988 <- p224r63_1988[[4]] - p224r63_1988[[3]]
+plot(dvi1988)
+
+#pure water will be fully absorbed in ndvi but if it has organisms it will show up at least a little
+
+#now to see the difference
+
+difdvi <- dvi1988 - dvi2011
+cl <- colorRampPalette(c('blue', 'white', 'red'))(100)
+plot(difdvi, col=cl)
